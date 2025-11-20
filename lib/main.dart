60594d47e0af1e390ad5b1782632676c3c9bc935
RocK111e <romanmeg9@gmail.com>
2025-11-20 19:58:43 +0200

    fixed not launching in chrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // lib/main.dart
 
 import 'package:event_diary2/firebase/analytics.dart';
-
 import '/page/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'page/login_page.dart';
@@ -9,31 +8,43 @@ import 'page/signup_page.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'dart:ui';
+import 'package:flutter/foundation.dart'; // Required for kIsWeb and defaultTargetPlatform
 
+// 1. Import the generated options file
+import 'firebase_options.dart'; 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
-  FlutterError.onError = (FlutterErrorDetails errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
+  // 2. Pass the options to initializeApp
+  // This fixes the "FirebaseOptions cannot be null" error on Web and Linux
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
+  // 3. Wrap Crashlytics in a check. 
+  // Crashlytics is NOT supported on Web and will crash your app if you try to use it there.
+  if (!kIsWeb && (defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.iOS)) {
+    FlutterError.onError = (FlutterErrorDetails errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
 
+    PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
+
+  // Analytics is supported on Web, so this is fine to leave outside the check
   await analytics.setAnalyticsCollectionEnabled(true);
 
   await analytics.logEvent(
     name: 'test_event'
   );
 
-
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
